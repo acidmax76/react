82ed710acb1118c201726cd5580e0fdd0040b876
acid_max <acidmax76@gmail.com>
2021-09-09 20:54:51 +0300

    init

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UI from '@ya.praktikum/react-developer-burger-ui-components';
+
+function AppHeader(){
+    return (
+        <header className="App-header">
+            UI
+        </header>
+    );
+}
+
+export default AppHeader;
