abdda9a7216000e1752655b3ace7900a0f8ba72d
acid_max <acidmax76@gmail.com>
2021-11-30 18:38:38 +0300

    подготовка к релизу

--- cypress/integration/all-in-one.spec.js
+++ cypress/integration/all-in-one.spec.js
@@ -1,47 +1,123 @@
-describe('Проверяем всю цепочку заказа бургера', function() {
-    before(()=> {
-        cy.viewport('macbook-13');
-        cy.clearCookie('accessToken');
-    });
-    it('Сайт должен быть доступен на localhost:3000', function() {
+describe('Проверяем всю цепочку заказа бургера', function () {
+    before(() => {
         cy.visit('http://localhost:3000');
-        cy.url().should('include','http://localhost:3000/');
+        cy.viewport('macbook-13');
     });
+    beforeEach(() => {
+
+            cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients.json'});
+            cy.intercept('POST', 'api/auth/login', {fixture: 'login.json'}).as('login');
+            cy.intercept('POST', 'api/orders', {fixture: 'order.json'}).as('order');
+        }
+    );
+
+    it('Проверяем что булок в конструкоре нет', () => {
+        cy.get('[class^="BurgerConstructor_constructor__item"]>div').should('contain.text', 'Перетащите сюда булку');
+    });
+
+    it('Проверяем что начинки в конструкоре нет', () => {
+        cy.get('[class^="BurgerConstructor_constructor__list"]>div').should('contain.text', 'Перетащите сюда начинку');
+    });
+
+    it('Перетаскиваем ингредиент булок', () => {
+        cy.get('[data-tab-id="buns"]>div[class^="TabIngredients"]>a:nth-child(1)').as('bun');
+        cy.get('[class^="BurgerConstructor_constructor__"').as('target');
+        cy.get('@bun').drag('@target').then((success) => {
+            assert.isTrue(success)
+        });
+    });
+
+    it('Перетаскиваем ингредиент соусы', () => {
+        cy.get('[data-tab-id="sauce"]>div[class^="TabIngredients"]>a:nth-child(1)').drag('[class^="BurgerConstructor_constructor__"').then((success) => {
+            assert.isTrue(success)
+        });
+    });
+
+    it('Проверяем, что булка есть в конструкторе', () => {
+        cy.get('[class^="BurgerConstructor_constructor__item"]>div>span>span[class^=constructor-element__text]').contains('Булка 1(верх)').should('exist');
+    });
+
+    it('Проверяем, что ингредиенты есть в конструкторе', () => {
+        cy.get('[class^="BurgerConstructor_constructor__list"]>li>div[class^="constructor-element"]>span>span[class^=constructor-element__text]').contains('Соус 1').should('exist');
+    });
+
+    it('Проверяем, что есть сумма заказа 2600', () => {
+        cy.get('[class^="BurgerConstructor_constructor__price__"]>span').should('contain.text', '2600');
+    });
+
+    it('Нажимаем кнопку оформить заказ', () => {
+        cy.get('[class^="BurgerConstructor_constructor__footer__"]>span>button').click();
+    });
+
+    it('Нас должно перкинуть на страницу логина', () => {
+        cy.url().should('include', '/login');
+    });
+
+    it('Вводим верный логин и пароль', () => {
+        cy.get('[class="input__icon input__icon-action"]').first().click();
+        cy.get('input[type="email"]').type("test22@test.ru").should('have.value', 'test22@test.ru');
+        cy.get('input[type="password"]').type('password').should('have.value', 'password');
+        cy.get('button[class^="button_button__"]').first().should('contain.text', 'Войти').click();
+    });
+
+    it('Проверяем есть ли в запросе данные', () => {
+        cy.wait('@login').its('request.body').should('deep.equal', {
+            "email": "test22@test.ru",
+            "password": "password"
+        })
+    })
+
+    it('Нажимаем кнопку оформить заказ', () => {
+        cy.get('[class^="BurgerConstructor_constructor__footer__"]>span>button').click();
+
+    });
+
+    it('Проверяем есть ли в запросе данные', () => {
+        cy.wait('@order').its('request.body').should('deep.equal', {
+            ingredients: ['3', '1', '1']
+        })
+    });
+
     it('Должно открыться модальное окно', () => {
-        cy.get('[class^=TabIngredients_ingredients__card__]').first().as('ingredient');
         cy.get('#react-modals').as('modal');
-        cy.get('@ingredient').click();
         cy.get('[class^=Modal_wrapper__]').first().should('exist');
-        cy.url().should('include','/ingredients/');
     });
-    it('Это модальное окно ингредиентов', () => {
-        cy.get('[class^=Modal_title__]').first().should('contain.text', 'Детали ингредиента');
+
+    it('Должен появится номер заказа 111111', () => {
+        cy.get('[class^="OrderDetails_digital__"').contains('111111').should('exist');
     });
-    it('В нем есть данные ингредиента', () => {
-        cy.get('[class^=ingredient_image__]').first().should('exist');
-        cy.get('[class^=ingredient_description__]').first().should('exist');
-        cy.get('[class^=ingredient_digits__]').first().should('exist');
-    });
+
     it('Закрытие модального окна', () => {
         cy.get('#react-modals').as('modal');
         cy.get('[class^="Modal_close__"]>svg').first().click();
         cy.get('@modal').should('not.contain.html');
-        cy.url().should('include','http://localhost:3000');
+        cy.url().should('include', 'http://localhost:3000');
     });
-    it('Переходим на страницу логина',()=>{
-        cy.visit('http://localhost:3000/login');
-        cy.url().should('include', '/login');
+    it('Подождем секунду чтобы не мелькало',()=>{
+        cy.wait(1000);
     });
-    it('Вводим верный логин и пароль',()=>{
-        cy.get('[class="input__icon input__icon-action"]').first().click();
-        cy.get('input[type="email"]').type("acidmax76@icloud.com").should('have.value', 'acidmax76@icloud.com');
-        cy.get('input[type="password"]').type('vfrc1111').should('have.value', 'vfrc1111');
-        cy.get('button[class^="button_button__"]').first().should('contain.text', 'Войти').click();
+    it('При клике на ингредиент должно открыться модальное окно', () => {
+        cy.get('[class^=TabIngredients_ingredients__card__]').first().as('ingredient');
+        cy.get('#react-modals').as('modal');
+        cy.get('@ingredient').click();
+        cy.get('[class^=Modal_wrapper__]').first().should('exist');
+        cy.url().should('include', '/ingredients/');
     });
-    it('Проверяем после успешного логина должны перейти на главную',()=>{
-        cy.url().should('include','http://localhost:3000/');
-    })
 
+    it('Это модальное окно ингредиентов', () => {
+        cy.get('[class^=Modal_title__]').contains('Детали ингредиента').should('exist');
+    });
+
+    it('В нем есть данные ингредиента Булка 1', () => {
+        cy.get('[class^=ingredient_description__]').contains('Булка 1').should('exist');
+    });
+
+    it('Закрытие модального окна', () => {
+        cy.get('#react-modals').as('modal');
+        cy.get('[class^="Modal_close__"]>svg').first().click();
+        cy.get('@modal').should('not.contain.html');
+        cy.url().should('include', 'http://localhost:3000');
+    });
 
 
 });
--- cypress/integration/is-available.spec.js
+++
@@ -1,9 +0,0 @@
-describe(' Запущен ли сайт', function() {
-    it('Сайт должен быть доступен на localhost:3000', function() {
-        cy.viewport('macbook-13');
-        cy.visit('http://localhost:3000');
-        cy.url().should('include','http://localhost:3000/');
-    });
-
-
-});
